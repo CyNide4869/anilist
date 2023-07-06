4035d8b7e7b26f91589403296911dfbb4f76e311
Cy <74846420+cynide4869@users.noreply.github.com>
2023-07-07 01:31:40 +0530

    Code is still dogshit but everything is working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import logging
 import time
 import requests
 import questionary
@@ -7,8 +8,8 @@ from pathlib import Path
 from urllib.parse import urlparse
 from dotenv import load_dotenv, set_key
 
-
 load_dotenv()
+logging.basicConfig(filename="alm.log", level=logging.DEBUG)
 
 CLIENT_ID = os.getenv('CLIENT_ID')
 AUTH_URL = f'https://anilist.co/api/v2/oauth/authorize?client_id={CLIENT_ID}&response_type=token'
@@ -48,6 +49,7 @@ def userAuth():
         # print(fragments_split)
         ACCESS_TOKEN = fragments_split[0].split('=')[1]
         set_key('.env', 'ACCESS_TOKEN', ACCESS_TOKEN)
+        logging.info("Authenticated User")
 
 def getAuthUserId():
     query = '''
@@ -63,6 +65,7 @@ def getAuthUserId():
     res = requests.post(API, json={'query': query, 'variables': variables}, headers=headers)
     data = res.json()
     print(f"\nLogged in as {data['data']['Viewer']['name']}")
+    logging.info("Fetching User ID")
     return data['data']['Viewer']['id']
 
 def storeUserMediaList(list_type, status):
@@ -133,19 +136,32 @@ def storeUserMediaList(list_type, status):
     while flag:
         res = requests.post(API, json={'query': query, 'variables': variables}, headers=headers)
         data = res.json()
+
+        error = data.get("errors")
+        if error:
+            break
+
         flag = data['data']['Page']['pageInfo']['hasNextPage']
         print("Page No: ", variables['page'])
+        logging.info(f"Downloading List {list_type} {status} PAGE {variables['page']}")
 
         mediaList += data['data']['Page']['mediaList']
         variables['page'] += 1
     
-    with open(f'./lists/{list_type.lower()}_{status.lower()}.json', 'w', encoding=FORMAT) as f:
-        obj = {}
-        obj['total'] = len(mediaList)
-        obj['mediaList'] = mediaList
-        print('Total: ', len(mediaList))
-        json.dump(obj, f, indent=4, ensure_ascii=False)
-        # sleepProgress(1)
+    if error:
+        print("[ERROR] Check Logs")
+        logging.error(logging.error(json.dumps(error)))
+        input("Enter to continue...")
+    else:        
+        with open(f'./lists/{list_type.lower()}_{status.lower()}.json', 'w', encoding=FORMAT) as f:
+            obj = {
+                "total": len(mediaList),
+                "mediaList": mediaList
+            }
+            print('Total: ', len(mediaList))
+            logging.info(f"Finished Downloading, SIZE {len(mediaList)}")
+            json.dump(obj, f, indent=4, ensure_ascii=False)
+            logging.info("Saved to file.")
 
 def deleteCompleteMediaList(list_type, status):
     deleteQuery = '''
@@ -156,10 +172,6 @@ def deleteCompleteMediaList(list_type, status):
     }
     '''
 
-    variables = {
-        'id': 0
-    }
-
     with open(f'./lists/{list_type.lower()}_{status.lower()}.json', 'r', encoding=FORMAT) as f:
         data = json.load(f)
     
@@ -167,39 +179,59 @@ def deleteCompleteMediaList(list_type, status):
         'deleted': []
     }
 
-    for entry in data['mediaList']:
-        variables['id'] = entry['id']
+    mediaList = data.get("mediaList")
+
+    for entry in mediaList:
+        variables = {
+            "id": entry['id']
+        }
 
         res = requests.post(API, json={'query': deleteQuery, 'variables': variables}, headers=headers)
         rem = res.headers['X-RateLimit-Remaining']
         resp = res.json()
+
+        error = resp.get("errors")
+        if error:
+            print(f"[ERROR] Check Logs")
+            logging.error(f"[ERROR] {json.dumps(error)}")
+            break
+
         print('Remaining requests: ', rem)
+        logging.info("Remaining Req: {rem}")
 
         if rem == '0':
             print('Deleted: ', len(deleted['deleted']))
             print('Hit rate limit, waiting 30s...')
+            logging.info("Hit Rate Limit")
             sleepProgress(30)
         else:
-            resp['title'] = entry['media']['title']
-            resp['status'] = entry['status']
-            resp['type'] = entry['media']['type']
-            resp['format'] = entry['media']['format']
-            print(json.dumps(resp, indent=4))
-            deleted['deleted'].append(resp)
+            title = entry['media']['title']
+            entry_type = entry['media']['type']
+            entry_format = entry['media']['format']
+            print(f"[DELETED] {title.get('romaji')} {entry_type} {entry_format}")
+            logging.info(f"[DELETED] {title.get('romaji')} {entry_type} {entry_format}")
+            deleted['deleted'].append({
+                "title": title,
+                "status": entry["status"],
+                "type": entry_type,
+                "format": entry_format
+            })
 
-    with open(DELETED_JSON, 'r+', encoding=FORMAT) as f:
-        try:
-            obj = json.load(f)
-        except Exception as e:
-            print(e)
-            obj = deleted
-        else:
-            obj['deleted'] += deleted['deleted']
-        finally:
-            f.seek(0)
-            json.dump(obj, f, indent=4, ensure_ascii=False)
+    if not error:
+        with open(DELETED_JSON, 'r+', encoding=FORMAT) as f:
+            try:
+                obj = json.load(f)
+            except Exception as e:
+                print(e)
+                obj = deleted
+            else:
+                obj['deleted'] += deleted['deleted']
+            finally:
+                f.seek(0)
+                json.dump(obj, f, indent=4, ensure_ascii=False)
 
     print('Deleted: ', len(deleted['deleted']))
+    logging.info(f"Delete: {len(deleted.get('deleted'))}")
 
 def saveMediaList(list_type, status):
     query = '''
@@ -233,18 +265,20 @@ def saveMediaList(list_type, status):
     with open(f'./lists/{list_type.lower()}_{status.lower()}.json', 'r', encoding=FORMAT) as f:
         data = json.load(f)
     
-    for entry in data['mediaList']:
+    mediaList = data.get("mediaList")
+
+    for entry in mediaList:
         variables = {
             'mediaId': entry["mediaId"],
             'status': entry["status"],
             'score': entry["score"],
             'progress': entry["progress"],
-            'progressVolumes': 0 if entry["progressVolumes"] == None else entry["progressVolumes"],
+            'progressVolumes': 0 if not entry["progressVolumes"] else entry["progressVolumes"],
             'repeat': entry["repeat"],
             'private': entry["private"],
-            'notes': entry["notes"],
+            'notes': None if not entry["notes"] else entry["notes"],
             'hiddenFromStatusLists': entry["hiddenFromStatusLists"],
-            'customLists': [x["name"] for x in entry["customLists"] if x["enabled"] == True],
+            'customLists': None if not entry["customLists"] else  [x["name"] for x in entry["customLists"] if x["enabled"] == True],
             'startedAt': entry["startedAt"],
             'completedAt': entry["completedAt"]
         }
@@ -252,14 +286,24 @@ def saveMediaList(list_type, status):
         res = requests.post(API, json={'query': query, 'variables': variables}, headers=headers)
         rem = res.headers['X-RateLimit-Remaining']
         resp = res.json()
-        print('Remaining requests: ', rem)
 
-        if rem == '0':
-            print('Hit rate limit, waiting 30s...')
-            sleepProgress(30)
+        error = resp.get("errors")
+
+        if error:
+            print(f"[ERROR] Check Logs")
+            logging.error(json.dumps(error))
         else:
-            print(json.dumps(resp, indent=4, ensure_ascii=False))
-    exit()
+            print('Remaining requests: ', rem)
+            logging.info("Remaining Req: {rem}")
+
+            if rem == '0':
+                print('Hit rate limit, waiting 30s...')
+                logging.info("Hit Rate Limit")
+                sleepProgress(30)
+            else:
+                savedMedia = resp.get("data").get("SaveMediaListEntry").get("media")
+                print(f"[UPDATED] {savedMedia.get('title').get('romaji')} {savedMedia.get('type')} {savedMedia.get('format')}")
+                logging.info(json.dumps(resp))
 
 def main():
     userAuth()
@@ -321,13 +365,11 @@ def main():
 
         if choice == 1:
             storeUserMediaList(list_type, status)
-            input("\n\nSaved lists to ./lists\nEnter to continue..")
 
         elif choice == 2:
             if not DELETED_JSON.exists():
                 DELETED_JSON.touch()
             deleteCompleteMediaList(list_type, status)
-            input("\n\nLogged lists to ./lists\nEnter to continue..")
 
         elif choice == 3:
             confirm = questionary.confirm(f"Ensure {list_type.lower()}_{status.lower()}.json is in ./lists", qmark=">>").ask()
@@ -340,6 +382,8 @@ def main():
             exit()
         else:
             pass
+
+        input("\nLogged lists to ./lists\nEnter to continue..")
 
 if __name__ == '__main__':
     main()
